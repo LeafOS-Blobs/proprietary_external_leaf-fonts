15e76a78678fc689c0f29697bd35f8c501f2299a
SamarV-121 <samarvispute121@pm.me>
2023-05-01 10:52:12 +0530

    fonts: Add fonts.mk
    Change-Id: I4a83558cd8cd498a05dd24e57c1c5147ff8691b1

---
+++ fonts.mk
@@ -0,0 +1,34 @@
+# Copyright (C) 2023 The LeafOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Fonts
+PRODUCT_PACKAGES += \
+    GoogleSans-Italic.ttf \
+    GoogleSans-Regular.ttf \
+    HarmonyOS-Sans-BlackItalic.ttf \
+    HarmonyOS-Sans-Black.ttf \
+    HarmonyOS-Sans-BoldItalic.ttf \
+    HarmonyOS-Sans-Bold.ttf \
+    HarmonyOS-Sans-Italic.ttf \
+    HarmonyOS-Sans-LightItalic.ttf \
+    HarmonyOS-Sans-Light.ttf \
+    HarmonyOS-Sans-MediumItalic.ttf \
+    HarmonyOS-Sans-Medium.ttf \
+    HarmonyOS-Sans-Regular.ttf \
+    HarmonyOS-Sans-ThinItalic.ttf \
+    HarmonyOS-Sans-Thin.ttf \
+    Inter-VF.ttf \
+    Linotte.ttf \
+    Manrope-VF.ttf \
+    OppoSans-En-Regular.ttf
