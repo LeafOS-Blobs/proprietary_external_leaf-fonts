37f658e43abceb088449002df1564e6b8a9b8175
Tim Zimmermann <tim@linux4.de>
2024-03-23 13:50:22 +0100

    fonts: Add Chococooky font
    Change-Id: I25f4da3683f1c70239c81559ad93ccc4b707b72b

--- fonts.mk
+++ fonts.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 The LeafOS Project
+# Copyright (C) 2023-2024 The LeafOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +14,7 @@
 
 # Fonts
 PRODUCT_PACKAGES += \
+    Chococooky.ttf \
     GoogleSans-Italic.ttf \
     GoogleSans-Regular.ttf \
     HarmonyOS-Sans-BlackItalic.ttf \
